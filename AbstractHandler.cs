9e3252059d02d4d13c6c72326ee3ef4fe51ec35d
Alrighter <59889888+Alrighter@users.noreply.github.com>
2020-11-26 13:21:09 +0200

    Initial commit

---
+++ AbstractHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Lab6
+{
+    public class AbstractHandler
+    {
+        private string _way;
+        private string _filename;
+        
+
+        public AbstractHandler(){}
+
+        public void constructor(string way, string filename)
+        {
+            this._way = way;
+            this._filename = filename;
+        }
+
+        public void Open()
+        {
+            File.Open(_way + _filename, FileMode.Open);
+        }
+
+        public void Create()
+        {
+            File.Create(_way + _filename);
+        }
+
+        public void Edit()
+        {
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            string path;
+            string filename;
+
+            AbstractHandler file = new AbstractHandler();
+
+            Console.Write("Lab6\nYou can:\n1) Open\n2) Create\n3) Edit\n\nEnter your answer:");
+            answer = Convert.ToInt32(Console.ReadKey());
+            Console.Write("Type the path of the file:");
+            path = Console.ReadLine();
+            Console.Write("Type filename including file extension:");
+            filename = Console.ReadLine();
+
+            file.constructor(filename,path);
+
+            switch (answer)
+            {
+                case 1:
+                    file.Open();
+                    break;
+                case 2:
+                    file.Create();
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    Console.WriteLine("Type the right answer");
+                    break;
+            }
+
+        }
+    }
+}
